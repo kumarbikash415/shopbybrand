4e935da1198e97c940a722bd52077fa0239d27ae
bikas <bikash kumar>
2018-01-17 16:51:53 +0530

    __DIR__

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php 
+  \Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistra::MODULE,
+	  'Dotsquares_shopbybrand',
+	    __DIR__
+	    
+  );
